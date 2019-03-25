c1713eaa35dc5f33d1b3b3b1603f5599aa6ba61b
dskrenta <dskrenta@gmail.com>
2019-03-25 03:15:49 -0700

    constants

--- server/resolvers/feedResolver.js
+++ server/resolvers/feedResolver.js
@@ -2,13 +2,15 @@
 
 const fetch = require('node-fetch');
 
+const { TRUSTED_NEWS_SOURCES } = require('../utils/constants');
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config({path: `${__dirname}/../../.env`});
 }
 
 async function feedResolver() {
   try {
-    const res = await fetch(`https://newsapi.org/v2/top-headlines?apiKey=${process.env.NEWS_API_KEY}&sources=abc-news,ars-technica,associated-press,bbc-news,bloomberg,business-insider,cbs-news,cnbc,cnn,engadget,financial-times,fortune,google-news,hacker-news,mashable,msnbc,national-geographic,national-review,nbc-news,new-scientist,newsweek,new-york-magazine,politico,recode,reddit-r-all,reuters,techcrunch,the-economist,the-hill,the-huffington-post,the-new-york-times,the-next-web,the-telegraph,the-verge,the-wall-street-journal,the-washington-post,the-washington-times,time,usa-today,wired`);
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?apiKey=${process.env.NEWS_API_KEY}&sources=${TRUSTED_NEWS_SOURCES}`);
     const data = await res.json();
 
     const results = data.articles.map(article => {
