8b891c555961a2584426d0b09c92fcf96149d433
dskrenta <dskrenta@gmail.com>
2019-04-03 14:08:28 -0700

    frontend updates

---
+++ web/src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p><span style={{fontFamily: 'Times New Roman'}}>The Daily</span> <span style={{fontFamily: 'Impact'}}>How<span style={{color: 'red'}}>lix</span></span></p>
+        <p style={{fontFamily: 'Times New Roman'}}>David's Feed</p>
+      </div>
+    );
+  }
+}
+
+export default App;
