c38bc59087c62afe4e548dd579a86abef6a48e96
dskrenta <dskrenta@gmail.com>
2019-09-09 16:41:36 -0600

    cors update

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ const PORT = 3000;
 const app = express();
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(express.static('public'));
 
 async function init() {
