5ef63b69819c2295cd12bd80a5a509738ff36875
David Skrenta <dskrenta@gmail.com>
2020-02-12 11:37:55 -0800

    removed legacy dir

--- legacy/server/utils/constants.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  TRUSTED_NEWS_SOURCES: 'abc-news,ars-technica,associated-press,bbc-news,bloomberg,business-insider,cbs-news,cnbc,cnn,engadget,financial-times,fortune,google-news,hacker-news,mashable,msnbc,national-geographic,national-review,nbc-news,new-scientist,newsweek,new-york-magazine,politico,recode,reddit-r-all,reuters,techcrunch,the-economist,the-hill,the-huffington-post,the-new-york-times,the-next-web,the-telegraph,the-verge,the-wall-street-journal,the-washington-post,the-washington-times,time,usa-today,wired'
-};
--- legacy/server/utils/generateResolver.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function generateResolver(fn) {
-  return async (obj, args, context) => {
-    return await fn({ obj, args, context, esClient: context.esClient, admin: context.admin });
-  }
-}
-
-module.exports = generateResolver;
